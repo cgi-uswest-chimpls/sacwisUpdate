7e50a8fe89ef757b7de7569bf2163707704e87da
granjan28 <gyan_ranjan28@yahoo.com>
2018-07-18 21:14:00 -0500

    Changes for sacwis update

--- src/main/java/com/cgi/uswest/chimpls/sacwisUpdate/SacwisUpdateApplication.java
+++ src/main/java/com/cgi/uswest/chimpls/sacwisUpdate/SacwisUpdateApplication.java
@@ -2,11 +2,18 @@ package com.cgi.uswest.chimpls.sacwisUpdate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class SacwisUpdateApplication {
+public class SacwisUpdateApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SacwisUpdateApplication.class, args);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 	}
 }
--- src/main/java/com/cgi/uswest/chimpls/sacwisUpdate/SacwisUpdateController.java
+++ src/main/java/com/cgi/uswest/chimpls/sacwisUpdate/SacwisUpdateController.java
@@ -8,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
-
 
 @Controller 
 @RequestMapping(path="/sacwisupdate")
@@ -21,7 +19,7 @@ public class SacwisUpdateController {
 	
 	private SacwisUpdateRepository sacwisUpdateRepository;
 	
-	@GetMapping(path="/add")
+	@RequestMapping(path = "/add", method = RequestMethod.POST)
 	public @ResponseBody String addNewSacwisUpdate (
 			//@RequestParam BigDecimal id_sacwis_update,
 			@RequestParam BigDecimal id_grp,
